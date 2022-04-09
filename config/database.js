7aa7a3d9aeb4fffe9f9951c5178289bfd0c04ae6
David Morales <dmorales474@gmail.com>
2022-04-09 13:10:28 +0200

    Loading .env file for DB credentials

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 import Sequelize from "sequelize";
-
+import dotenv from "dotenv";
+dotenv.config();
 
 const user = process.env.MYSQL_USER || "root";
 const password = process.env.MYSQL_PASSWORD || "root";
+
 // connect to db
 
-export default new Sequelize('dice_game', user, password, { dialect: 'mysql' });
+export default new Sequelize(' ', user, password, { dialect: 'mysql' });
 
